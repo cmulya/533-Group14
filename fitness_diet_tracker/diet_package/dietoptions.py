071ce13a8420874eb6147c6f729155d4df244185
Baldeep Dhada <bdhada@student.ubc.ca>
2023-11-25 17:35:32 -0800

    codeFrame

--- fitness_diet_tracker/diet_package/dietoptions.py
+++ fitness_diet_tracker/diet_package/dietoptions.py
@@ -1,1 +1,17 @@
-#generate meal recipes
+#generate Diet Options
+
+class DietOptions(PlanOptions):
+    def __init__(self):
+        pass
+    def generate_vegan_meal(self):
+        pass
+    def generate_vegetarian_meal(self):
+        pass
+    def generate_meat_meal(self):
+        pass
+
+def main():
+    input("Select one of the following Diet Options (Vegan/Vegetarian/Meat): ")
+    pass
+
+main()
