d36db960663578cef77f5a1122b13e4e667e4078
Christopher Mulya <christophermulya@Christophers-MacBook-Air.local>
2023-11-29 14:29:45 -0800

    draft class planoptions functions

--- fitness_diet_tracker/diet_package/planoptions.py
+++ fitness_diet_tracker/diet_package/planoptions.py
@@ -1,22 +1,52 @@
 #superclass
 
 class PlanOptions:
-    def __init__(self):
-        pass
-    def calculate_bmi(self):
-        pass
+    def __init__(self, height, weight, age, gender, activity_level, weight_goal):
+        self.height = height
+        self.weight = weight
+        self.age = age
+        self.gender = gender
+        self.activity_level = activity_level
+        self.weight_goal = weight_goal
+        
+    def calculate_bmr(self):
+        if self.gender.lower() == 'female':
+            # BMR for women
+            self.bmr = 655.1 + (9.563 * self.weight) + (1.850 * self.height) - (4.676 * self.age)
+        elif self.gender.lower() == 'male':
+            # BMR for men
+            self.bmr = 66.47 + (13.75 * self.weight) + (5.003 * self.height) - (6.755 * self.age)
+        else:
+            print("Invalid gender input. BMI calculation not possible.")
+            return None
+
+        return self.bmr
+    
     def calculate_tdee(self):
-        pass
+        if self.bmr is not None:
+            if self.activity_level.lower() == 'low':
+                self.tdee = self.bmr * 1.2
+            elif self.activity_level.lower() == 'moderate':
+                self.tdee = self.bmr * 1.55
+            elif self.activity_level.lower() == 'high':
+                self.tdee = self.bmr * 1.9
+            else:
+                print("Invalid activity level input. TDEE calculation not possible.")
+                return None
+
+            return self.tdee
+    
     def calculate_target_cal(self):
-        pass
-
-def main():
-    input("What is your height? ")
-    input("What is your weight? ")
-    input("What is your age? ")
-    input("What is your gender? ")
-    input("What is your activity level? ")
-    input("What is your weight goal (gain/loss)? ")
-    pass
-
-main()
+        #calculate target calorie intake per day based on weight goal
+        #if 'gain', then the target calorie intake = self.tdee * 110%
+        #if 'loss', then the target calorie intake = self.tdee * 90%
+        if self.tdee is not None:
+            if self.weight_goal.lower() == 'gain':
+                self.target_cal = self.tdee * 1.1
+            elif self.weight_goal.lower() == 'loss':
+                self.target_cal = self.tdee * 0.9
+            else:
+                print("Invalid weight goal input. Target calories calculation not possible.")
+                return None
+            
+            return self.target_cal
